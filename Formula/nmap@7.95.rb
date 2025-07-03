3eff9378f067f345b90662104055593e7d18d6ac
Wallace Winfrey <1929725+wallacewinfrey@users.noreply.github.com>
2025-07-03 12:13:12 -0600

    added formula

---
+++ Formula/nmap@7.95.rb
@@ -0,0 +1,74 @@
+class NmapAT795 < Formula
+  desc "Port scanning utility for large networks"
+  homepage "https://nmap.org/"
+  url "https://nmap.org/dist/nmap-7.95.tar.bz2"
+  sha256 "e14ab530e47b5afd88f1c8a2bac7f89cd8fe6b478e22d255c5b9bddb7a1c5778"
+  license :cannot_represent
+  revision 1
+  head "https://svn.nmap.org/nmap/"
+
+  livecheck do
+    url "https://nmap.org/download"
+    regex(/href=.*?nmap[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "liblinear"
+  depends_on "libssh2"
+  # Check supported Lua version at https://github.com/nmap/nmap/tree/master/liblua.
+  depends_on "lua"
+  depends_on "openssl@3"
+  depends_on "pcre2"
+
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+  uses_from_macos "libpcap"
+  uses_from_macos "zlib"
+
+  conflicts_with "cern-ndiff", "ndiff", because: "both install `ndiff` binaries"
+  conflicts_with "nping", because: "both install `nping` binaries"
+
+  def install
+    ENV.deparallelize
+
+    libpcap_path = if OS.mac?
+      MacOS.sdk_path/"usr/"
+    else
+      Formula["libpcap"].opt_prefix
+    end
+
+    args = %W[
+      --with-liblua=#{Formula["lua"].opt_prefix}
+      --with-libpcre=#{Formula["pcre2"].opt_prefix}
+      --with-openssl=#{Formula["openssl@3"].opt_prefix}
+      --with-libpcap=#{libpcap_path}
+      --without-nmap-update
+      --disable-universal
+      --without-zenmap
+    ]
+
+    system "./configure", *args, *std_configure_args
+    system "make" # separate steps required otherwise the build fails
+    system "make", "install"
+
+    bin.glob("uninstall_*").map(&:unlink) # Users should use brew uninstall.
+    return unless (bin/"ndiff").exist? # Needs Python
+
+    # We can't use `rewrite_shebang` here because `detected_python_shebang` only works
+    # for shebangs that start with `/usr/bin`, but the shebang we want to replace
+    # might start with `/Applications` (for the `python3` inside Xcode.app).
+    inreplace bin/"ndiff", %r{\A#!.*/python(\d+(\.\d+)?)?$}, "#!/usr/bin/env python3"
+  end
+
+  def caveats
+    on_macos do
+      <<~EOS
+        If using `ndiff` returns an error about not being able to import the ndiff module, try:
+          chmod go-w #{HOMEBREW_CELLAR}
+      EOS
+    end
+  end
+
+  test do
+    system bin/"nmap", "-p80,443", "google.com"
+  end
+end
